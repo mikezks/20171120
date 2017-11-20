e48d0dc1cf9b6f2ec2d18b262270f5cf70ea0f03
unknown <michael.zikes@intauria.com>
2017-11-20 22:53:03 +0100

    TypeScript

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import './typescript-demo/demo';
 
 if (environment.production) {
   enableProdMode();
